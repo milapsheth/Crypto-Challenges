82eaaccac060f5de1503a6b0973753d9585b8ae6
Milap Sheth <milap.sheth2010@gmail.com>
2016-06-06 01:01:55 -0400

    Completed challenge 3 again

--- README.md
+++ README.md
@@ -4,15 +4,19 @@ My attempt at completing [the crypto challenges](http://cryptopals.com/) and pla
 
 ## Completed challenges
 
-1. Set 1
+* Set 1
    - [x] Challenge 1
    - [x] Challenge 2
    - [x] Challenge 3
    - [ ] Challenge 4
    - [ ] Challenge 5
    - [ ] Challenge 6
-2. Set 2
-3. Set 3
-4. Set 4
-5. Set 5
-6. Set 6
+   - [ ] Challenge 7
+   - [ ] Challenge 8
+* Set 2
+* Set 3
+* Set 4
+* Set 5
+* Set 6
+* Set 7
+* Set 8
--- src/set1/xor_cipher.clj
+++ src/set1/xor_cipher.clj
@@ -17,17 +17,22 @@
 
 
 ;; 12 most frequently occuring charaters in English
-(def char-freq [\e \t \a \o \i \n \s \h \r \d \l \u])
+(def char-freq [\e \t \a \o \space \i \n \s \h \r \d \l \u])
 
 
 (defn score-match
   [match]
   (def char-count (reduce #(update %1 %2 (fnil inc 0)) {} match))
   (reduce' #(+ %1 (* %3 (get char-count %2 0))) 0
-           char-freq (range 26 14 -1)))
+           char-freq (range 26 13 -1)))
+
+(defn rotate-by-char
+  [byte-lst byte]
+  (map #(char (bit-xor % byte)) byte-lst))
 
 ;; Input hex string "1b4f36a" -> decrypted ascii
 (defn brute-force
+  "Brute force through all rotations and output the one with best score"
   [cdata]
   (def data (map int (hexstr-to-str cdata)))
   (loop [score 0
@@ -36,13 +41,31 @@
     
     (if (empty? nums)
       (clojure.string/join matched-data)
-      (let [match (map #(char (bit-xor % (first nums))) data)
+      (let [match (rotate-by-char data (first nums))
              new-score (score-match match)]
         (recur (if (> new-score score) new-score score)
                (if (> new-score score) match matched-data)
                (rest nums))))))
 
 (defn decrypt
-  [data]
-  (def char-count (reduce #(update-in %1 [%2] (fnil inc 0)) (seq data)))
-  )
+  "Get most frequent character and then assuming
+  it is one of the most frequent character, 
+  output the data with best score"
+  [cdata]
+  (def data (map int (hexstr-to-str cdata)))
+  (def char-count (reduce #(update %1 %2 (fnil inc 0)) {} data))
+
+  (let [max-char (key (apply max-key val char-count))]
+    (loop [freq-lst char-freq
+           best-score 0
+           best-match data]
+
+      (if (empty? freq-lst)
+        (clojure.string/join best-match)
+
+        (let [cipher-key (bit-xor max-char (int (first freq-lst)))
+              match (rotate-by-char data cipher-key)
+              score (score-match match)]
+          (recur (rest freq-lst)
+                 (if (> score best-score) score best-score)
+                 (if (> score best-score) match best-match)))))))
