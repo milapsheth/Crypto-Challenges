2d09a0ee0df2d2e8ddc5724b177e8f798c9748ce
Milap Sheth <milap.sheth2010@gmail.com>
2016-06-23 22:18:11 -0400

    Updated testing

--- test/set2/break_ecb_simple_test.clj
+++ test/set2/break_ecb_simple_test.clj
@@ -12,14 +12,18 @@
   (aes/encrypt (concat plaintext unknown-string) random-cipher-key :ecb))
 
 
-(deftest break-ecb-basic-test
+(deftest ^:parallel break-ecb-basic-test
   (testing "Failed to break ECB mode on sample string"
     (let [unknown-string (map int "UNKNOWN STRING")]
       (is (= unknown-string
              (sut/break-ecb #(oracle-encrypt % unknown-string)))))))
 
+;; Actual challenge test
+;; Not run as part of the suite as it takes a bit of time to run
+
 (def unknown-string (u/base64-to-byte' "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK"))
 
+#_
 (deftest break-ecb-simple-test
   (testing "Failed to break ECB mode(simple)"
     (is (= unknown-string
