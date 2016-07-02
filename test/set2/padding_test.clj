42ea4933fbd9f1645e12648132f099d3f1988efe
Milap Sheth <milap.sheth2010@gmail.com>
2016-07-02 17:35:54 -0400

    Fix tests

--- test/set2/padding_test.clj
+++ test/set2/padding_test.clj
@@ -5,14 +5,14 @@
 (def plaintext (seq "YELLOW SUBMARINE"))
 
 
-(deftest ^:parallel padding-test
+(deftest padding-test
   (testing "Failed to pad correctly"
     (is (= plaintext
            (-> plaintext
                (pad-plaintext 16 :pkcs7)
                (unpad-plaintext 16 :pkcs7))))))
 
-(deftest ^:parallel padding-test2
+(deftest padding-test2
   (testing "Failed padding test"
     (is (= (map int "YELLOW SUBMARINE\04\04\04\04")
            (-> (map int plaintext)
